e44283e17b9124e266e4602e2ec835b290012d35
Kirill Sysoev <k.sysoev@me.com>
2024-10-13 11:51:48 +0800

    Refactor composer_test.go to use testify/assert for assertions and improve test readability

--- pkg/core/composer/composer_test.go
+++ pkg/core/composer/composer_test.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func makeParser() func([]byte) (map[string]any, error) {
@@ -21,24 +23,20 @@ func makeParser() func([]byte) (map[string]any, error) {
 }
 
 func TestComposer_WaitResponse_Success(t *testing.T) {
-	expectedData := `{"Params":"param1,param2","ReqID":1234}`
 	composer := New()
 	respChan := make(chan []byte, 1)
 	parser := makeParser()
 	ctx := context.Background()
 
-	respChan <- []byte(expectedData)
+	respChan <- []byte(`{"Params":"param1,param2","ReqID":1234}`)
 
-	go composer.Wait(ctx, parser, respChan)
+	composer.Wait(ctx, parser, respChan)
 
 	resp, err := composer.Compose()
-	if err != nil {
-		t.Fatalf("expected no error, got %v", err)
-	}
 
-	if resp["Params"] != "param1,param2" {
-		t.Fatalf("expected param1,param2, got %s", resp["Params"])
-	}
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, "param1,param2", resp["Params"])
 }
 
 func TestComposer_WaitResponse_ParseError(t *testing.T) {
@@ -52,8 +50,9 @@ func TestComposer_WaitResponse_ParseError(t *testing.T) {
 	composer.Wait(ctx, makeParser(), respChan)
 
 	_, err := composer.Compose()
+
 	if !strings.HasPrefix(err.Error(), "fail to parse response: invalid character") {
-		t.Fatalf("expected error: %s, got something else: %s", err.Error(), err)
+		t.Errorf("expected error: %s, got something else: %s", err.Error(), err)
 	}
 }
 
@@ -68,7 +67,6 @@ func TestComposer_WaitResponse_ContextCancelled(t *testing.T) {
 	composer.Wait(ctx, makeParser(), respChan)
 
 	res, err := composer.Compose()
-	if err == nil {
-		t.Fatalf("expected error, got nil. While response was: %s", res)
-	}
+	assert.Nil(t, res)
+	assert.Error(t, err)
 }
