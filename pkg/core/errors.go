ae2396774ca83cfe6fe5ba7fa2bba27544ac2110
Kirill Sysoev <k.sysoev@me.com>
2024-10-13 14:50:38 +0800

    Refactoring errors

---
+++ pkg/core/errors.go
@@ -0,0 +1,30 @@
+package core
+
+import "encoding/json"
+
+type APIError struct {
+	Code    string          `json:"code"`
+	Message string          `json:"message"`
+	Details json.RawMessage `json:"details,omitempty"`
+}
+
+func NewAPIError(code, message string, details json.RawMessage) *APIError {
+	return &APIError{
+		Code:    code,
+		Message: message,
+		Details: details,
+	}
+}
+
+func (e *APIError) Error() string {
+	return e.Message
+}
+
+func (e *APIError) ApiError() json.RawMessage {
+	bytes, err := json.Marshal(e)
+	if err != nil {
+		panic("failed to marshal APIError: " + err.Error())
+	}
+
+	return bytes
+}
