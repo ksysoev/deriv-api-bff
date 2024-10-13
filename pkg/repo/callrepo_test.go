98a681a68e01b7da4cc6c6cb4a0345155f326eb1
Kirill Sysoev <k.sysoev@me.com>
2024-10-13 11:18:32 +0800

    Updates tests for calls repo

--- pkg/repo/callrepo_test.go
+++ pkg/repo/callrepo_test.go
@@ -1,133 +1,119 @@
 package repo
 
-// import (
-// 	"html/template"
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/ksysoev/deriv-api-bff/pkg/core/validator"
+	"github.com/stretchr/testify/assert"
+)
 
-// func TestNewCallsRepository(t *testing.T) {
-// 	tests := []struct {
-// 		cfg     *CallsConfig
-// 		name    string
-// 		wantErr bool
-// 	}{
-// 		{
-// 			name: "valid config",
-// 			cfg: &CallsConfig{
-// 				Calls: []CallConfig{
-// 					{
-// 						Method: "testMethod",
-// 						Params: map[string]string{"param1": "value1"},
-// 						Backend: []BackendConfig{
-// 							{
-// 								FieldsMap:       map[string]string{"field1": "value1"},
-// 								ResponseBody:    "responseBody1",
-// 								RequestTemplate: "template1",
-// 								Allow:           []string{"allow1"},
-// 							},
-// 						},
-// 					},
-// 				},
-// 			},
-// 			wantErr: false,
-// 		},
-// 		{
-// 			name: "invalid template",
-// 			cfg: &CallsConfig{
-// 				Calls: []CallConfig{
-// 					{
-// 						Method: "testMethod",
-// 						Params: map[string]string{"param1": "value1"},
-// 						Backend: []BackendConfig{
-// 							{
-// 								FieldsMap:       map[string]string{"field1": "value1"},
-// 								ResponseBody:    "responseBody1",
-// 								RequestTemplate: "{{.InvalidTemplate",
-// 								Allow:           []string{"allow1"},
-// 							},
-// 						},
-// 					},
-// 				},
-// 			},
-// 			wantErr: true,
-// 		},
-// 	}
+func TestNewCallsRepository(t *testing.T) {
+	tests := []struct {
+		cfg     *CallsConfig
+		name    string
+		wantErr bool
+	}{
+		{
+			name: "valid config",
+			cfg: &CallsConfig{
+				Calls: []CallConfig{
+					{
+						Method: "testMethod",
+						Params: validator.Config{"param1": {Type: "string"}},
+						Backend: []BackendConfig{
+							{
+								FieldsMap:       map[string]string{"field1": "value1"},
+								ResponseBody:    "responseBody1",
+								RequestTemplate: "template1",
+								Allow:           []string{"allow1"},
+							},
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid template",
+			cfg: &CallsConfig{
+				Calls: []CallConfig{
+					{
+						Method: "testMethod",
+						Params: validator.Config{"param1": {Type: "value1"}},
+						Backend: []BackendConfig{
+							{
+								FieldsMap:       map[string]string{"field1": "value1"},
+								ResponseBody:    "responseBody1",
+								RequestTemplate: "{{.InvalidTemplate",
+								Allow:           []string{"allow1"},
+							},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+	}
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			got, err := NewCallsRepository(tt.cfg)
-// 			if tt.wantErr {
-// 				assert.Error(t, err)
-// 			} else {
-// 				assert.NoError(t, err)
-// 				assert.NotNil(t, got)
-// 				assert.Contains(t, got.calls, "testMethod")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewCallsRepository(tt.cfg)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, got)
+				assert.Contains(t, got.calls, "testMethod")
+			}
+		})
+	}
+}
+func TestGetCall(t *testing.T) {
+	repo, err := NewCallsRepository(&CallsConfig{
+		Calls: []CallConfig{
+			{
+				Method: "testMethod",
+				Params: validator.Config{"param1": {Type: "string"}},
+				Backend: []BackendConfig{
+					{
+						FieldsMap:       map[string]string{"field1": "value1"},
+						ResponseBody:    "responseBody1",
+						RequestTemplate: "template1",
+						Allow:           []string{"allow1"},
+					},
+				},
+			},
+		},
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, repo)
 
-// 				callConfig := got.calls["testMethod"]
-// 				assert.NotNil(t, callConfig)
-// 				assert.Contains(t, callConfig.Requests, "responseBody1")
+	tests := []struct {
+		name   string
+		method string
+		found  bool
+	}{
+		{
+			name:   "existing method",
+			method: "testMethod",
+			found:  true,
+		},
+		{
+			name:   "non-existing method",
+			method: "nonExistingMethod",
+			found:  false,
+		},
+	}
 
-// 				requestConfig := callConfig.Requests["responseBody1"]
-// 				assert.NotNil(t, requestConfig)
-// 				assert.Equal(t, "responseBody1", requestConfig.ResponseBody)
-// 				assert.Equal(t, []string{"allow1"}, requestConfig.Allow)
-// 				assert.Equal(t, map[string]string{"field1": "value1"}, requestConfig.FieldMap)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			callConfig := repo.GetCall(tt.method)
 
-// 				tmpl, err := template.New("request").Parse("template1")
-// 				assert.NoError(t, err)
-// 				assert.Equal(t, tmpl.Tree.Root.String(), requestConfig.Tmplt.Tree.Root.String())
-// 			}
-// 		})
-// 	}
-// }
-// func TestGetCall(t *testing.T) {
-// 	repo, err := NewCallsRepository(&CallsConfig{
-// 		Calls: []CallConfig{
-// 			{
-// 				Method: "testMethod",
-// 				Params: map[string]string{"param1": "value1"},
-// 				Backend: []BackendConfig{
-// 					{
-// 						FieldsMap:       map[string]string{"field1": "value1"},
-// 						ResponseBody:    "responseBody1",
-// 						RequestTemplate: "template1",
-// 						Allow:           []string{"allow1"},
-// 					},
-// 				},
-// 			},
-// 		},
-// 	})
-// 	assert.NoError(t, err)
-// 	assert.NotNil(t, repo)
-
-// 	tests := []struct {
-// 		name   string
-// 		method string
-// 		found  bool
-// 	}{
-// 		{
-// 			name:   "existing method",
-// 			method: "testMethod",
-// 			found:  true,
-// 		},
-// 		{
-// 			name:   "non-existing method",
-// 			method: "nonExistingMethod",
-// 			found:  false,
-// 		},
-// 	}
-
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			callConfig := repo.GetCall(tt.method)
-
-// 			if tt.found {
-// 				assert.NotNil(t, callConfig)
-// 			} else {
-// 				assert.Nil(t, callConfig)
-// 			}
-// 		})
-// 	}
-// }
+			if tt.found {
+				assert.NotNil(t, callConfig)
+			} else {
+				assert.Nil(t, callConfig)
+			}
+		})
+	}
+}
