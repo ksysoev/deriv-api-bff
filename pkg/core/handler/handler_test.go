b79d6a14330b3263dd41de2495463d24a90876b6
Kirill Sysoev <k.sysoev@me.com>
2024-10-15 21:00:49 +0800

    Adds test case for handler constuctor

--- pkg/core/handler/handler_test.go
+++ pkg/core/handler/handler_test.go
@@ -1,1 +1,22 @@
 package handler
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	validator := NewMockValidator(t)
+	processors := []RenderParser{NewMockRenderParser(t)}
+	compFactory := func() WaitComposer {
+		return NewMockWaitComposer(t)
+	}
+
+	handler := New(validator, processors, compFactory)
+
+	assert.NotNil(t, handler)
+	assert.Equal(t, validator, handler.validator)
+	assert.Equal(t, processors, handler.processors)
+	assert.NotNil(t, handler.newComposer)
+}
