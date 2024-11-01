fdd89e8f6342b15155d0b666af750d9ee726b9da
Kirill Sysoev <k.sysoev@me.com>
2024-11-01 16:33:57 +0800

    Replace validation logic with validator

--- pkg/core/validator/validator.go
+++ pkg/core/validator/validator.go
@@ -2,6 +2,7 @@ package validator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 
 	"github.com/santhosh-tekuri/jsonschema/v5"
@@ -14,7 +15,6 @@ type FieldSchema struct {
 }
 
 type FieldValidator struct {
-	fields     Config
 	jsonSchema *jsonschema.Schema
 }
 
@@ -23,12 +23,6 @@ type FieldValidator struct {
 // It returns a pointer to a FieldValidator and an error.
 // It returns an error if any field in the configuration has an unknown type.
 func New(cfg Config) (*FieldValidator, error) {
-	for field, fieldConfig := range cfg {
-		if fieldConfig.Type != "string" && fieldConfig.Type != "number" && fieldConfig.Type != "boolean" {
-			return nil, fmt.Errorf("unknown type %s for field %s", fieldConfig.Type, field)
-		}
-	}
-
 	schema := map[string]interface{}{
 		"type":                 "object",
 		"properties":           make(map[string]interface{}),
@@ -53,7 +47,6 @@ func New(cfg Config) (*FieldValidator, error) {
 	}
 
 	return &FieldValidator{
-		fields:     cfg,
 		jsonSchema: val,
 	}, nil
 }
@@ -63,61 +56,28 @@ func New(cfg Config) (*FieldValidator, error) {
 // It returns an error if there are validation errors, including missing required fields or fields that are not allowed.
 // If the data contains fields not defined in the validator or if required fields are missing, it adds these errors to the validation error.
 func (v *FieldValidator) Validate(data map[string]any) error {
-	errValidation := NewValidationError()
-
-	for field, config := range v.fields {
-		if value, ok := data[field]; ok {
-			if err := v.validateField(config, value); err != nil {
-				errValidation.AddError(field, err)
-			}
-		} else {
-			errValidation.AddError(field, fmt.Errorf("field %s is missing", field))
-		}
-	}
-
-	for field := range data {
-		if _, ok := v.fields[field]; !ok {
-			errValidation.AddError(field, fmt.Errorf("field %s is not allowed", field))
-		}
-	}
-
-	if errValidation.HasErrors() {
-		return errValidation.APIError()
-	}
-
 	err := v.jsonSchema.Validate(data)
 
-	if err != nil {
+	var errValidation *jsonschema.ValidationError
+
+	switch {
+	case errors.As(err, &errValidation):
+		e := NewValidationError()
+
+		for _, v := range errValidation.Causes {
+			e.AddError(
+				fmt.Sprintf("params%s", v.InstanceLocation),
+				fmt.Errorf("%s", v.Message),
+			)
+		}
+
+		if e.HasErrors() {
+			return e.APIError()
+		}
+
+		return err
+	case err != nil:
 		return fmt.Errorf("failed to validate data: %w", err)
-	}
-
-	return nil
-}
-
-// validateField checks if the provided value matches the expected type defined in the FieldSchema.
-// It takes a config of type *FieldSchema and a value of type any.
-// It returns an error if the value does not match the expected type specified in the config.
-// Possible error conditions include:
-// - The value is not of type string when config.Type is "string".
-// - The value is not of type float64 when config.Type is "number".
-// - The value is not of type bool when config.Type is "boolean".
-// - The config.Type is unknown.
-func (v *FieldValidator) validateField(config *FieldSchema, value any) error {
-	switch config.Type {
-	case "string":
-		if _, ok := value.(string); !ok {
-			return fmt.Errorf("field %s is not a string", value)
-		}
-	case "number":
-		if _, ok := value.(float64); !ok {
-			return fmt.Errorf("field %s is not an int", value)
-		}
-	case "boolean":
-		if _, ok := value.(bool); !ok {
-			return fmt.Errorf("field %s is not a bool", value)
-		}
-	default:
-		return fmt.Errorf("unknown field type %s", config.Type)
 	}
 
 	return nil
