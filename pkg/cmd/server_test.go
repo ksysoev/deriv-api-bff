e72ff9cd3c32686a54f514e2d0e29750c53a791f
Kirill Sysoev <k.sysoev@me.com>
2024-11-03 17:49:08 +0800

    Revive cmd server tests

--- pkg/cmd/server_test.go
+++ pkg/cmd/server_test.go
@@ -1,100 +1,45 @@
 package cmd
 
-// var validPartialConfig = `
-// server:
-//   listen: ":0"
-// `
+import (
+	"context"
+	"testing"
 
-// func TestRunServer(t *testing.T) {
-// 	cfg := &config.Config{
-// 		Server: api.Config{
-// 			Listen: ":0",
-// 		},
-// 	}
+	"github.com/ksysoev/deriv-api-bff/pkg/api"
+	"github.com/ksysoev/deriv-api-bff/pkg/config"
+	"github.com/stretchr/testify/assert"
+)
 
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	cancel()
+func TestRunServer(t *testing.T) {
+	callsPath := createTempConfigFile(t, callsConfig)
 
-// 	err := runServer(ctx, cfg)
+	cfg := &Config{
+		Server: api.Config{
+			Listen: ":0",
+		},
+		APISource: config.SourceConfig{
+			Path: callsPath,
+		},
+	}
 
-// 	assert.NoError(t, err)
-// }
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
 
-// func TestRunServer_WithWatcher(t *testing.T) {
-// 	cfg := &config.Config{}
-// 	source := config.NewFileSource(createTempConfigFile(t, validPartialConfig))
+	err := runServer(ctx, cfg)
 
-// 	err := source.Init(cfg)
-// 	assert.NoError(t, err)
+	assert.NoError(t, err)
+}
 
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	cancel()
+func TestRunServer_Error(t *testing.T) {
+	cfg := &Config{
+		Server: api.Config{
+			Listen: ":0",
+		},
+	}
 
-// 	err = runServer(ctx, cfg)
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
 
-// 	assert.Nil(t, cfg.API.Calls)
-// 	assert.NoError(t, err)
+	err := runServer(ctx, cfg)
 
-// 	callsConfig := config.CallsConfig{
-// 		Calls: []config.CallConfig{
-// 			{
-// 				Method: "testMethod",
-// 				Params: validator.Config{"param1": validator.FieldSchema{Type: "string"}},
-// 				Backend: []*config.BackendConfig{
-// 					{
-// 						FieldsMap:       map[string]string{"field1": "value1"},
-// 						ResponseBody:    "responseBody1",
-// 						RequestTemplate: map[string]any{"template1": "t1"},
-// 						Allow:           []string{"allow1"},
-// 					},
-// 				},
-// 			},
-// 		},
-// 	}
-// 	newCfg := &config.Config{
-// 		Server: cfg.Server,
-// 		API:    callsConfig,
-// 	}
-// 	newCfgJSON, err := json.Marshal(newCfg)
-// 	assert.NoError(t, err)
-
-// 	newCfgJSONString := string(newCfgJSON)
-
-// 	createTempConfigFile(t, newCfgJSONString)
-// 	time.Sleep(1 * time.Second)
-
-// 	newCfg, err = source.GetConfigurations()
-// 	watchKeys := source.GetWatchKeys()
-
-// 	assert.NoError(t, err)
-// 	assert.NotEmpty(t, watchKeys)
-// 	assert.NotEmpty(t, newCfg.API.Calls)
-// 	assert.NotNil(t, watchKeys["api.calls"])
-// }
-
-// func TestRunServer_Error(t *testing.T) {
-// 	cfg := &config.Config{
-// 		Server: api.Config{
-// 			Listen: ":0",
-// 		},
-// 		API: config.CallsConfig{
-// 			Calls: []config.CallConfig{
-// 				{
-// 					Method: "GET",
-// 					Params: validator.Config{
-// 						"param": &validator.FieldSchema{
-// 							Type: "InvalidType",
-// 						},
-// 					},
-// 				},
-// 			},
-// 		},
-// 	}
-
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	cancel()
-
-// 	err := runServer(ctx, cfg)
-
-// 	assert.Error(t, err)
-// }
+	assert.Error(t, err)
+}
