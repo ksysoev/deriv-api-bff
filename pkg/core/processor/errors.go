39263d2d1ee8cb297553b0f8d565318073c5d2c3
Kirill Sysoev <k.sysoev@me.com>
2024-11-16 17:51:34 +0800

    Refactor NewAPIError to use json.RawMessage for input data and improve error handling in tests

--- pkg/core/processor/errors.go
+++ pkg/core/processor/errors.go
@@ -7,36 +7,26 @@ import (
 	"github.com/ksysoev/deriv-api-bff/pkg/core"
 )
 
+type errorData struct {
+	Code    string          `json:"code"`
+	Message string          `json:"message"`
+	Details json.RawMessage `json:"details,omitempty"`
+}
+
 // NewAPIError creates a new API error from the provided data.
 // It takes a single parameter data of type any, which is expected to be a map with keys "code", "message", and optionally "details".
 // It returns an error which is an instance of core.APIError if the data is in the expected format, or a descriptive error if the data format is incorrect.
 // It returns an error if the data is not a map, if the "code" or "message" fields are missing or not strings, or if the "details" field cannot be marshaled to JSON.
-func NewAPIError(data any) error {
-	err, ok := data.(map[string]any)
-	if !ok {
-		return fmt.Errorf("error data is not in expected format")
+func NewAPIError(data json.RawMessage) error {
+	var errData errorData
+
+	if err := json.Unmarshal(data, &errData); err != nil {
+		return fmt.Errorf("failed to unmarshal APIError data: %w", err)
 	}
 
-	code, ok := err["code"].(string)
-	if !ok {
-		return fmt.Errorf("error code is not in expected format")
-	}
-
-	message, ok := err["message"].(string)
-	if !ok {
+	if errData.Code == "" || errData.Message == "" {
 		return fmt.Errorf("error message is not in expected format")
 	}
 
-	var detailsData json.RawMessage
-
-	if details, ok := err["details"]; ok {
-		var err error
-
-		detailsData, err = json.Marshal(details)
-		if err != nil {
-			return fmt.Errorf("failed to marshal APIError details: %w", err)
-		}
-	}
-
-	return core.NewAPIError(code, message, detailsData)
+	return core.NewAPIError(errData.Code, errData.Message, errData.Details)
 }
