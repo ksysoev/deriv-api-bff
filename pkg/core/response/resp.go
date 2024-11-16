10351df3247b023b2ae4ea5b8c0ba547c71b9156
Kirill Sysoev <k.sysoev@me.com>
2024-11-16 11:07:33 +0800

    Creates container to carry response

---
+++ pkg/core/response/resp.go
@@ -0,0 +1,20 @@
+package response
+
+type Response struct {
+	filtered map[string]any
+	data     map[string]any
+}
+
+func New(data map[string]any, filtered map[string]any) *Response {
+	return &Response{
+		data: data,
+	}
+}
+
+func (r *Response) Body() map[string]any {
+	return r.data
+}
+
+func (r *Response) Filtered() map[string]any {
+	return r.filtered
+}
