19170c4e097a47b9c8f0b7746fcf7b31bfad267a
Kirill Sysoev <k.sysoev@me.com>
2024-11-16 16:22:03 +0800

    Updates composer tests

--- pkg/core/composer/composer_test.go
+++ pkg/core/composer/composer_test.go
@@ -16,7 +16,7 @@ func makeParser(t *testing.T) func([]byte) (*response.Response, error) {
 	t.Helper()
 
 	return func(data []byte) (*response.Response, error) {
-		var res map[string]any
+		var res map[string]json.RawMessage
 
 		if err := json.Unmarshal(data, &res); err != nil {
 			return nil, err
@@ -51,7 +51,7 @@ func TestComposer_Success(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.NotNil(t, resp)
-	assert.Equal(t, "param1,param2", resp["Params"])
+	assert.Equal(t, json.RawMessage(`"param1,param2"`), resp["Params"])
 }
 
 func TestComposer_Compose_ParseError(t *testing.T) {
